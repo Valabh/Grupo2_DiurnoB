cbac27ac7c4f1ca9b8b87f0d3816bfd42f794d26
andreflb <andreflb@sapo.pt>
2020-07-04 14:27:01 +0100

    g

--- Projecto_Final_Java/src/com/istec/pagina/Dashboard.java
+++ Projecto_Final_Java/src/com/istec/pagina/Dashboard.java
@@ -2,12 +2,14 @@ package com.istec.pagina;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,6 +20,8 @@ import javax.swing.border.EmptyBorder;
 
 import com.istec.main.dados;
 import com.istec.objetos.produto;
+import com.istec.objetos.vendedor;
+
 import javax.swing.JFileChooser;
 
 public class Dashboard extends JFrame {
@@ -30,27 +34,7 @@ public class Dashboard extends JFrame {
 	private JPanel hotdrinks;
 	private JPanel snacks;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Dashboard frame = new Dashboard();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-
-	 public Dashboard() {
+	public Dashboard() {
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setBounds(100, 100, 900, 641);
 				contentPane = new JPanel();
@@ -59,18 +43,18 @@ public class Dashboard extends JFrame {
 				setContentPane(contentPane);
 				contentPane.setLayout(null);
 				
-				//JButton Manage = new JButton("Manage");
-				//Manage.setBounds(20, 13, 117, 29);
-				//contentPane.add( Manage);
-				 //Manage.addMouseListener(new MouseAdapter() {
-					//@Override
-					//public void mouseClicked(MouseEvent arg0) {
-						//VendasPage vendasp = new VendasPage();
-						//vendasp.setVisible(true);
-						//dispose();
+				JButton Manage = new JButton("Manage");
+				Manage.setBounds(20, 13, 117, 29);
+				contentPane.add( Manage);
+				 Manage.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent arg0) {
+						vendas vendasp = new vendas();
+						vendasp.setVisible(true);
+						dispose();
 						
-					//}
-				//});
+					}
+				});
 		
     manage.setBounds(20, 13, 117, 29);
 	contentPane.add(manage);
@@ -111,7 +95,7 @@ public class Dashboard extends JFrame {
 	
 			JLabel lblNewLabel = new JLabel();
 			lblNewLabel.setBounds(0, 0, 40, 40);
-			//lblNewLabel.setIcon(new ImageIcon(new ImageIcon(p.getImagem()).getImage().getScaledInstance(lblNewLabel.getWidth(),lblNewLabel.getHeight(), Image.SCALE_DEFAULT)));
+			lblNewLabel.setIcon(new ImageIcon(new ImageIcon(p.getImagemdoProduto()).getImage().getScaledInstance(lblNewLabel.getWidth(),lblNewLabel.getHeight(), Image.SCALE_DEFAULT)));
 			Snacks.add(lblNewLabel);
 		}
 		}
@@ -130,7 +114,7 @@ public class Dashboard extends JFrame {
 			{	
 			JLabel lblNewLabel_2 = new JLabel();
 			lblNewLabel_2.setBounds(0, 0, 40, 40);
-			//lblNewLabel_2.setIcon(new ImageIcon(new ImageIcon(p.getImagem()).getImage().getScaledInstance(lblNewLabel_2.getWidth(),lblNewLabel_2.getHeight(), Image.SCALE_DEFAULT)));
+			lblNewLabel_2.setIcon(new ImageIcon(new ImageIcon(p.getImagemdoProduto()).getImage().getScaledInstance(lblNewLabel_2.getWidth(),lblNewLabel_2.getHeight(), Image.SCALE_DEFAULT)));
 			HotDrinks.add(lblNewLabel_2);
 		}
 	}
@@ -149,40 +133,40 @@ public class Dashboard extends JFrame {
 			if (p.getTipodeProduto().equals("Drinks")){
 						JLabel lblNewLabel_3 = new JLabel();
 						lblNewLabel_3.setBounds(0, 0, 40, 40);
-						//	lblNewLabel_3.setIcon(new ImageIcon(new ImageIcon(p.getImagem()).getImage().getScaledInstance(lblNewLabel_3.getWidth(),lblNewLabel_3.getHeight(), Image.SCALE_DEFAULT)));
+						lblNewLabel_3.setIcon(new ImageIcon(new ImageIcon(p.getImagemdoProduto()).getImage().getScaledInstance(lblNewLabel_3.getWidth(),lblNewLabel_3.getHeight(), Image.SCALE_DEFAULT)));
 						Drinks.add(lblNewLabel_3);
 					}
 				}		
 	 }
 				JLabel Addvendedor = new JLabel("Addvendedor");
 	Addvendedor.setBounds(767, 13, 86, 66);
-	//	Addvendedor.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\harsh\\git\\repository\\Projecto_Final_Java\\Image\\addUser.png").getImage().getScaledInstance(Addvendedor.getWidth(),Addvendedor.getHeight(), Image.SCALE_DEFAULT)));
+	Addvendedor.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\harsh\\git\\repository\\Projecto_Final_Java\\Image\\addUser.png").getImage().getScaledInstance(Addvendedor.getWidth(),Addvendedor.getHeight(), Image.SCALE_DEFAULT)));
 	contentPane.add(Addvendedor);
-	//Addvendedor.addMouseListener(new MouseAdapter() {
-	//	@Override
-	//	public void mouseClicked(MouseEvent arg0) {
-			//	vendedor vendores = new vendedor();
-	//		vendores.setVisible(true);
-	//		dispose();
-			
-	//	}
-	//});
+	Addvendedor.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			vendedor vendas = new vendedor();
+			vendas.setVisible(true);
+			dispose();
+			//adausdsadasd
+		}
+	});
 	Addvendedor.setBounds(771, 13, 98, 75);
 	contentPane.add(Addvendedor);
 	
 	JLabel Addproduto = new JLabel("Addproduto");
 	Addproduto.setBounds(758, 510, 98, 91);
-	//Addproduto.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\harsh\\git\\repository\\Projecto_Final_Java\\Image\\add.png").getImage().getScaledInstance(Addproduto.getWidth(),Addproduto.getHeight(), Image.SCALE_DEFAULT)));
+	Addproduto.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\harsh\\git\\repository\\Projecto_Final_Java\\Image\\add.png").getImage().getScaledInstance(Addproduto.getWidth(),Addproduto.getHeight(), Image.SCALE_DEFAULT)));
 					contentPane.add(Addproduto);
-					//	Addproduto.addMouseListener(new MouseAdapter() {
-					//	@Override
-					//public void mouseClicked(MouseEvent arg0) {
-					//		produto product = new produto();
-					//		product.setVisible(true);
-					//		dispose();
+					Addproduto.addMouseListener(new MouseAdapter() {
+						@Override
+						public void mouseClicked(MouseEvent arg0) {
+							produto product = new produto();
+							product.setVisible(true);
+							dispose();
 							
-					//	}
-					//});
+						}
+					});
 					
 					contentPane.add(Addproduto);
 					
@@ -193,21 +177,21 @@ public class Dashboard extends JFrame {
 					setResizable(false);
 					
 					 if(dados.getinstance().getprodutos() != null) { 
-							//  ArrayList<vendedor> vendedores = dados.getinstance().getvendedores();
+				        ArrayList<vendedor> vendedores = dados.getinstance().getvendedores();
 				        
 				        
-							//   for (vendedor v : vendedores) {
+				        for (vendedor v : vendedores) {
 			
-							//    	JLabel lblvendedor = new JLabel(v.getusername());         	
-							// 	lblvendedor.setSize(80, 80);
-							//	lblvendedor.setHorizontalAlignment(JLabel.CENTER);
-				        	//	lblvendedor.setVerticalTextPosition(JLabel.BOTTOM);
-							// 	lblvendedor.setHorizontalTextPosition(JLabel.CENTER);
-				        	//	Image imagemv = new ImageIcon(v.getImagemv()).getImage().getScaledInstance(lblvendedor.getWidth(),lblvendedor.getHeight(), Image.SCALE_DEFAULT);
-				        	//	lblvendedor.setIcon(new ImageIcon(imagemv));
-							//	panel.add(lblvendedor);
-				    		//
-							//}
+				        	JLabel lblvendedor = new JLabel(v.getusername());         	
+				        	lblvendedor.setSize(80, 80);
+				        	lblvendedor.setHorizontalAlignment(JLabel.CENTER);
+				        	lblvendedor.setVerticalTextPosition(JLabel.BOTTOM);
+				        	lblvendedor.setHorizontalTextPosition(JLabel.CENTER);
+				    		//Image imagemv = new ImageIcon(v.getImagemv()).getImage().getScaledInstance(lblvendedor.getWidth(),lblvendedor.getHeight(), Image.SCALE_DEFAULT);
+				    		//lblvendedor.setIcon(new ImageIcon(imagemv));
+				    		panel.add(lblvendedor);
+					
+					}
 				}
 		 }
 	}
